6473dabc6874d98d5a1843d3e04d6c58009c0dcd
Carlos Andres De La Rosa <kusanagi12002@gmail.com>
2025-08-21 14:22:21 -0500

    Initializes price calculator application
    Adds core functionality for calculating prices with tax.
    
    This commit introduces the foundational structure for a price calculator application.
    It includes:
    - File and command-line input/output management
    - Conversion of string inputs to float values
    - Concurrent processing of tax rates
    - JSON output of results

---
+++ concurrency/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/prices"
+)
+
+func main() {
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
+
+	for index, taxRate := range taxRates {
+		doneChans[index] = make(chan bool)
+		errorChans[index] = make(chan error)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		go priceJob.Process(doneChans[index], errorChans[index])
+
+		// if err != nil {
+		// 	fmt.Println("Could not process job")
+		// 	fmt.Println(err)
+		// }
+	}
+
+	for index := range taxRates {
+		select { // allows to wait on multiple channels and proceed when one of them is ready using case statements
+		case <-doneChans[index]:
+			fmt.Println("Job completed")
+		case err := <-errorChans[index]:
+			if err != nil {
+				fmt.Println("Error occurred:", err)
+			}
+		}
+	}
+}
